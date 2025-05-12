f3e7003895affc483b7f8c451803b0d493bc9dbb
Kaboom-18 <kaboomofficial18@gmail.com>
2025-05-12 13:33:37 +0545

    aboutus update

--- app/src/main/java/geq/kaboom/app/kaboot/Config.java
+++ app/src/main/java/geq/kaboom/app/kaboot/Config.java
@@ -24,8 +24,7 @@ public class Config {
   
   public final static String REPOURL = "https://raw.githubusercontent.com/Kaboom-18/Kaboot/refs/heads/main/Packages.json";
   
-  public final static String ABOUTUS = "Kaboot is a robust Linux emulation application developed by Kaboom, enabling a full Linux environment on Android devices—no root access required. It supports the latest Android versions and is specifically engineered to bypass W^X (Write XOR Execute) restrictions, ensuring maximum compatibility and stability.\n\n•Facebook\n-KA Boom \n\n•Email\n-Kaboomofficial18@gmail.com";
- 
+  public final static String ABOUTUS = "Kaboot is a robust Linux emulation application developed by Kaboom, enabling a full Linux environment on Android devices—no root access required. It supports the latest Android versions and is specifically engineered to bypass W^X (Write XOR Execute) restrictions, ensuring maximum compatibility and stability. Users can also use their custom repositories to run their own Linux or shell-based applications.\n\n•Facebook\n-KA Boom \n\n•Email\n-Kaboomofficial18@gmail.com";
   public static String getTmpDir(Context context){
       return (context.getCacheDir().getAbsolutePath());
   }
